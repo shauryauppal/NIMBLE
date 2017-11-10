b47672f32ea8fe4b423ca6586a27037f2a719db4
Shaurya <shauryauppal00111@gmail.com>
2017-11-10 21:45:54 +0530

    Crashlytics added to app, this automatically detects crash in app

--- app/src/main/java/com/example/shaur/nimblenavigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/shaur/nimblenavigationdrawer/MainActivity.java
@@ -24,6 +24,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
